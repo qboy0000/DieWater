6bf2ac53213ecab658d1a172b5d6ff3b910b75a8
qgw <qgw@jk.com>
2015-02-07 14:53:06 +0800

    添加背景层

---
+++ src/GameIndexLayer.js
@@ -0,0 +1,15 @@
+/**
+ * Created by QGW on 2015/2/7.
+ * GameIndex 页
+ * 预计先分两种方式（经典模式、计时模式）
+ */
+
+var GameIndexLayer = cc.Layer.extend({
+    onEnter:function(){
+        this._super();
+    },
+    ctor:function(){
+        this._super();
+    }
+
+})
